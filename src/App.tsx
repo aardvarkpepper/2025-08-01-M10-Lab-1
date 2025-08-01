ab78daa43fa378225abb3ae8ed6000c850789986
James Lee <aardvarkpepper@gmail.com>
2025-08-01 13:26:38 -0400

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App:React.FunctionComponent = ():React.ReactNode => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default App
